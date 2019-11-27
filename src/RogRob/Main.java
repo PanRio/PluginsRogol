7cc71526df57fe735025649cd1c5c8e6774ff5de
Roberto <roberto.laczak@gmail.com>
2019-11-27 22:09:48 +0100

    Obczaj.Test1

--- src/RogRob/Main.java
+++ src/RogRob/Main.java
@@ -1,22 +1,33 @@
 
 package RogRob;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
-//WYWYWYWYWYWYW
-    //PACZAJ
-    //TU
+public class Main extends JavaPlugin implements Listener {
 
+    ItemStack compass;
 
-    //MOWY PUSH
-
-    //bez xml
     @Override
     public void onEnable() {
+
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
+
+
+        ItemMeta meta = compass.getItemMeta();
+        meta.setDisplayName(ChatColor.GOLD + "ChestOpener");
+        compass.setItemMeta(meta);
+        compass = new ItemStack(Material.RABBIT_HIDE);
         super.onEnable();
     }
 
@@ -28,13 +39,28 @@ public class Main extends JavaPlugin {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        if(sender instanceof Player)
+        if(!(sender instanceof Player)) return false;
+
+        Player player = (Player) sender;
+
+        if(label.equalsIgnoreCase("getkey"))
         {
-            //SUPER
-            int a = 1;
+            player.getInventory().addItem(new ItemStack(compass));
+            player.sendMessage(ChatColor.GREEN + "Dostales kompass otwierania skrzyni");
         }
 
         return super.onCommand(sender, command, label, args);
+    }
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent event)
+    {
+        Player player = event.getPlayer();
+
+        if(player.getItemOnCursor().equals(compass))
+        {
+            player.sendMessage(ChatColor.BLUE + "Otwierasz Skrzynie Wirtualna");
+        }
     }
 }
 
