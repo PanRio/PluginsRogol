cc977a3c13abc907be60f663ff44df1f76d06159
Roberto <roberto.laczak@gmail.com>
2019-11-27 21:18:39 +0100

    Fratr

---
+++ src/RogRob/Main.java
@@ -0,0 +1,26 @@
+package RogRob;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+
+
+        return super.onCommand(sender, command, label, args);
+    }
+}
